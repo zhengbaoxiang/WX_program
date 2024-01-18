30056a481705373d4311335e14dd3478d679f3f2
zhengbaoxiang <zhengbaoxiang@github.com>
2024-01-18 20:00:24 +0800

    更新文件，删掉不需要的demo

--- myapp/utils/util.js
+++ myapp/utils/util.js
@@ -14,6 +14,16 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
+const shuffleArray = function (array =[]) {
+  console.log(array.length)
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 module.exports = {
-  formatTime
+  formatTime,
+  shuffleArray
 }
